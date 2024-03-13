3ddb94364c39700dd28df094234e395c2b6d519a
emanueldias0 <effdsd27@gmail.com>
2024-03-13 15:42:52 -0300

    atualização de código

--- js/script .js
+++ js/script .js
@@ -1,4 +1,4 @@
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const mario = document.querySelector('.mario');
     const pipe = document.querySelector('.pipe');
     const contadorDisplay = document.getElementById('contador');
@@ -16,42 +16,45 @@
         }, 500);
     }
 
+    // Event listener para clique ou toque (para dispositivos móveis)
+    
     const loop = setInterval(() => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
+        
         if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 70) {
             jogoAtivo = false; 
-
+            
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
-
+            
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
-
+            
             mario.src = './images/game-over.png';
             mario.style.width = '75px';
             mario.style.marginLeft = '50px';
-
+            
             clearInterval(loop);
             clearInterval(atualizacaoDePontos);
-
+            
             document.getElementById('finalScore').textContent = pontos;
             
             modal.style.display = 'block';
             restartBtn.style.display = 'block';
         }
     }, 10);
-
+    
     const atualizacaoDePontos = setInterval(() => {
         if (jogoAtivo) {
             pontos++;
             contadorDisplay.textContent = pontos;
         }
     }, 100);
-
+    
     document.addEventListener('keydown', jump);
-
+    document.addEventListener('click', jump);
+    
     restartBtn.addEventListener('click', () => {
         window.location.reload();
     });
